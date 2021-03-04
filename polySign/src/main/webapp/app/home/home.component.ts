81b0e787099a25ad5fb520cb3c73311976d9cde5
Aleck Bilounga <hyorickgerrard@gmail.com>
2021-03-04 16:06:53 +0100

    [CPTN] adding a base home page for the userEntity which is not finished yet

--- polySign/src/main/webapp/app/home/home.component.ts
+++ polySign/src/main/webapp/app/home/home.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'app/core/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+// import {Router} from "@angular/router";
+// import {FormBuilder} from "@angular/forms";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'jhi-home',
@@ -11,10 +14,21 @@ import { Account } from 'app/core/user/account.model';
 })
 export class HomeComponent implements OnInit {
   account: Account | null = null;
+  authSubscription?: Subscription;
+  authenticationError = false;
+  isAuth = false;
+  /* loginForm = this.formBuilder.group({
+    username: [''],
+    password: [''],
+    rememberMe: [false],
+  });*/
 
-  constructor(private accountService: AccountService, private loginService: LoginService) {}
+  constructor(private accountService: AccountService, private loginService: LoginService) // private router : Router,
+  // private formBuilder : FormBuilder
+  {}
 
   ngOnInit(): void {
+    // this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
     this.accountService.identity().subscribe(account => (this.account = account));
   }
 
--- polySign/src/main/webapp/app/home/home.module.ts
+++ polySign/src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { PolySignSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { UserHomeComponent } from 'app/user-home/user-home.component';
 
 @NgModule({
   imports: [PolySignSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, UserHomeComponent],
 })
 export class PolySignHomeModule {}
